e6061ec00369315c79e9ce99533af42a88db897c
sundongyang <21192557+youlingpixiu@users.noreply.github.com>
2022-12-08 16:34:58 +0800

    增加

---
+++ page/demo.vue
@@ -0,0 +1,37 @@
+<!--
+ * @Copyright: 2016-2022 航天神舟智慧系统技术有限公司
+ * @Descripttion: 页面描述
+ * @Author: sundongyang
+ * @Date: 2022-12-06 14:15:52
+ * @LastEditors: sundongyang
+ * @LastEditTime: 2022-12-06 14:16:03
+-->
+<template>
+  <div>
+    嵌入的vue
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WintersunshineDemo',
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
