553443c63bd15edcc9361620e87bcfd7d395b36e
SunDongYang <sundongyang1221@163.com>
2024-07-02 22:00:21 +0800

    添加quark资源

---
+++ assets/quark/quark.js
@@ -0,0 +1,382 @@
+var quark_json = [
+  {
+    typeName: "网络热门短剧",
+    typeList: [
+      {
+        name: "W-网络短剧-度盘",
+        url: "https://pan.baidu.com/s/1TWvB8wTeco_MwFJiD2xp1A?pwd=4vf4",
+      },
+
+      {
+        name: "W-网络短剧-夸克",
+        url: "https://pan.quark.cn/s/3be1feef21d0",
+      },
+    ]
+  },
+  {
+    typeName: "教育资源",
+    typeList: [
+      { name: "高考真题", url: "https://pan.quark.cn/s/9636bbc995a9" },
+      {
+        name: "大学英语四六级考试必备资料包",
+        url: "https://pan.quark.cn/s/a7b1e5d70b6d",
+      },
+      { name: "24年高考", url: "https://pan.quark.cn/s/ad4389e542ed" },
+      {
+        name: "初中专区合集2",
+        url: "https://pan.quark.cn/s/82cb7d0ccea1",
+      },
+      {
+        name: "小学专区合集1",
+        url: "https://pan.quark.cn/s/c24a61ad6a4a",
+      },
+      {
+        name: "大学四六级",
+        url: "https://pan.quark.cn/s/90c4bacc457f",
+      },
+      {
+        name: "婴幼少儿教育",
+        url: "https://pan.quark.cn/s/2cc3f20ffca0",
+      },
+      { name: "竞赛专区4", url: "https://pan.quark.cn/s/4281d3bb19e7" },
+      {
+        name: "口语要素8000句--备课",
+        url: "https://pan.quark.cn/s/f81500ac620e",
+      },
+    ],
+  },
+  {
+    typeName: "E-影视集合",
+    typeList: [
+      { name: "E-影视集合", url: "https://pan.quark.cn/s/1e4c0ba15dbe" },
+      { name: "PD强人", url: "https://pan.quark.cn/s/793ad4dd1ae8" },
+      { name: "大主宰年番", url: "https://pan.quark.cn/s/cc81ff02d5e2" },
+      { name: "谍之心", url: "https://pan.quark.cn/s/3a116ab44962" },
+      { name: "郎君不如意", url: "https://pan.quark.cn/s/50e3e3666691" },
+      { name: "《长相思》", url: "https://pan.quark.cn/s/5437de153c14" },
+      { name: "《莲花楼》", url: "https://pan.quark.cn/s/bd85a701ef60" },
+      { name: "W雾山五行", url: "https://pan.quark.cn/s/84e81997e677" },
+      { name: "斯Ba达Ke斯", url: "https://pan.quark.cn/s/0dbc5f4cc31d" },
+      { name: "《潜行者》", url: "https://pan.quark.cn/s/92e11de1a070" },
+      {
+        name: "超异能族  (2023) 官方中字",
+        url: "https://pan.quark.cn/s/cbdf0953ca7d",
+      },
+      {
+        name: "P 破毒强人丨港剧丨2023丨",
+        url: "https://pan.quark.cn/s/18ae7880337c",
+      },
+      { name: "扫毒3", url: "https://pan.quark.cn/s/f478b47ab995" },
+      {
+        name: "《正义联盟:战争世界》流媒体1080P",
+        url: "https://pan.quark.cn/s/a3d81b806ad7",
+      },
+      { name: "明日之战", url: "https://pan.quark.cn/s/5c3c57f55dc6" },
+      { name: "夺命隧道.2019", url: "https://pan.quark.cn/s/86d3dc31cd89" },
+      {
+        name: "新哥斯拉.东京陷落[中英字幕]",
+        url: "https://pan.quark.cn/s/86b64f190986",
+      },
+      {
+        name: "中华百年电影漫展237部",
+        url: "https://pan.quark.cn/s/4a5a6d3d9261",
+      },
+      { name: "聊斋专题", url: "https://pan.quark.cn/s/b9fbfdb64035" },
+      {
+        name: "聊斋艳谭2 五通神主演黄秋生-1991",
+        url: "https://pan.quark.cn/s/e6141f94e697",
+      },
+      {
+        name: "F父辈的荣耀(1)",
+        url: "https://pan.quark.cn/s/b2a995e4979b",
+      },
+      {
+        name: "李丽珍珍藏老电影系列",
+        url: "https://pan.quark.cn/s/aca377248eb7",
+      },
+      {
+        name: "Z《中国婚礼 好事成双季》2023 真人秀 12集全",
+        url: "https://pan.quark.cn/s/e3405b37df8f",
+      },
+      {
+        name: "J. 疾速营救2023动作惊悚犯罪4K",
+        url: "https://pan.quark.cn/s/73c01dcbb755",
+      },
+    ],
+  },
+  {
+    typeName: "A-中医世家",
+    typeList: [
+      { name: "A-中医世家", url: "https://pan.quark.cn/s/aeb4829e9ec2" },
+      { name: "补肾壮阳", url: "https://pan.quark.cn/s/309bced37390" },
+      {
+        name: "《中国药酒配方大全》",
+        url: "https://pan.quark.cn/s/2f3a9f4630e9",
+      },
+      { name: "药酒大全", url: "https://pan.quark.cn/s/d5964530e3d0" },
+      {
+        name: "中医内科辨证学",
+        url: "https://pan.quark.cn/s/bea6e8201b94",
+      },
+      { name: "四时脉法", url: "https://pan.quark.cn/s/59bfdfbcc4d9" },
+      {
+        name: "全国名老中医临床经验高级讲习班(1)",
+        url: "https://pan.quark.cn/s/a1267dfd7cdc",
+      },
+      {
+        name: "国医大师验案良方",
+        url: "https://pan.quark.cn/s/e12786f966a2",
+      },
+      {
+        name: "全国名老中医临床经验高级讲习班",
+        url: "https://pan.quark.cn/s/73af040d349e",
+      },
+      {
+        name: "近代名老中医经验集",
+        url: "https://pan.quark.cn/s/00ace6190007",
+      },
+      { name: "名师经方讲录", url: "https://pan.quark.cn/s/113e848c0c01" },
+      {
+        name: "岭南中医药名家",
+        url: "https://pan.quark.cn/s/72fd484a9697",
+      },
+    ],
+  },
+  {
+    typeName: "C-个人传记",
+    typeList: [
+      { name: "C-个人传记", url: "https://pan.quark.cn/s/9f7337939802" },
+      { name: "袁世凯全集", url: "https://pan.quark.cn/s/58517c4c7097" },
+      {
+        name: "《李鸿章全集》",
+        url: "https://pan.quark.cn/s/023572693bbe",
+      },
+      {
+        name: "张之洞全集",
+        url: "https://pan.quark.cn/s/6823cae75cb6",
+      },
+      { name: "左宗棠全集", url: "https://pan.quark.cn/s/e7c67f11c674" },
+      { name: "梁思成全集", url: "https://pan.quark.cn/s/66776570cb1f" },
+      { name: "廖耀湘", url: "https://pan.quark.cn/s/cdde4cfa48e2" },
+      { name: "王耀武", url: "https://pan.quark.cn/s/000342f17657" },
+      { name: "胡宗南", url: "https://pan.quark.cn/s/c948166424f9" },
+      { name: "陈诚", url: "https://pan.quark.cn/s/79975fd8a1e8" },
+      { name: "胡琏", url: "https://pan.quark.cn/s/6d5cc9aea625" },
+      { name: "薛岳", url: "https://pan.quark.cn/s/982550afb443" },
+      { name: "张灵甫", url: "https://pan.quark.cn/s/9f8ba030048d" },
+      { name: "杜聿明", url: "https://pan.quark.cn/s/edd2952796e9" },
+      {
+        name: "张上将自忠画传",
+        url: "https://pan.quark.cn/s/169ff44fb584",
+      },
+      { name: "张自忠", url: "https://pan.quark.cn/s/283279d1d18a" },
+      { name: "孙立人", url: "https://pan.quark.cn/s/8c6140a651e9" },
+    ],
+  },
+  {
+    typeName: "D-红色记忆",
+    typeList: [
+      { name: "D-红色记忆", url: "https://pan.quark.cn/s/a67379681387" },
+      { name: "重探抗戰史", url: "https://pan.quark.cn/s/73ad2bc29090" },
+      {
+        name: "中国人民解放军英雄模范名录",
+        url: "https://pan.quark.cn/s/d6c88e261093",
+      },
+      {
+        name: "天下之脊：刘邓大军征程志略",
+        url: "https://pan.quark.cn/s/717a8ce99f19",
+      },
+      {
+        name: "51035. 中国人民解放军将帅名录（第一集）.pdf",
+        url: "https://pan.quark.cn/s/5ef3acd10ab3",
+      },
+      {
+        name: "51037. 中国人民解放军将帅名录（第三集）.pdf",
+        url: "https://pan.quark.cn/s/604532af2c91",
+      },
+      {
+        name: "51036. 中国人民解放军将帅名录（第二集）.pdf",
+        url: "https://pan.quark.cn/s/1377c8c9099d",
+      },
+      {
+        name: "中国人民解放军十大王牌军[全71集]",
+        url: "https://pan.quark.cn/s/f7f837dc2617",
+      },
+      { name: "红色聚焦", url: "https://pan.quark.cn/s/4e9d05ae81ac" },
+      { name: "红色将帅", url: "https://pan.quark.cn/s/b593173d082e" },
+      { name: "《红色中华》", url: "https://pan.quark.cn/s/db4e8c3283d7" },
+      { name: "红旗飘飘", url: "https://pan.quark.cn/s/344611f7d87a" },
+      { name: "红色记忆", url: "https://pan.quark.cn/s/608107e263f1" },
+      {
+        name: "红色档案：延安时期文献档案汇编01-15",
+        url: "https://pan.quark.cn/s/3996168338ba",
+      },
+      { name: "将帅名录", url: "https://pan.quark.cn/s/475cb5a2fbc0" },
+      { name: "淮海战役", url: "https://pan.quark.cn/s/de7b8211e698" },
+    ],
+  },
+  {
+    typeName: "L-旅游攻略",
+    typeList: [
+      { name: "L-旅游攻略", url: "https://pan.quark.cn/s/af7c18258dc0" },
+
+      {
+        name: "美國《國家地理》125週年偉大瞬間系列叢書",
+        url: "https://pan.quark.cn/s/e6a40fff5bc3",
+      },
+      {
+        name: "全球220大最佳旅遊城市 ",
+        url: "https://pan.quark.cn/s/80e10c16fa73",
+      },
+      {
+        name: "一生必遊的225夢幻之旅 ",
+        url: "https://pan.quark.cn/s/dab6c9b27ffa",
+      },
+      {
+        name: "國家地理終極旅遊：一生必遊的500美食之旅",
+        url: "https://pan.quark.cn/s/146531deab62",
+      },
+      {
+        name: "红楼梦大观园复原地图 ",
+        url: "https://pan.quark.cn/s/4710a2137ddd",
+      },
+      {
+        name: "徐霞客旅行路线考察图集",
+        url: "https://pan.quark.cn/s/99c966493a85",
+      },
+      { name: "郑和航海图", url: "https://pan.quark.cn/s/64225d081b62" },
+      { name: "国内旅游攻略", url: "https://pan.quark.cn/s/a1b0637705b2" },
+
+      { name: "云南地区", url: "https://pan.quark.cn/s/d01085845cc8" },
+      { name: "华东地区", url: "https://pan.quark.cn/s/63bea2560949" },
+      { name: "华中地区", url: "https://pan.quark.cn/s/f2765ef95fb8" },
+      { name: "华北地区", url: "https://pan.quark.cn/s/b20d67740305" },
+      { name: "华南地区", url: "https://pan.quark.cn/s/18b4f0381dec" },
+      { name: "四川地区", url: "https://pan.quark.cn/s/38114663d515" },
+      { name: "山东地区", url: "https://pan.quark.cn/s/ee52f5ac51c4" },
+      { name: "江苏地区", url: "https://pan.quark.cn/s/cfccfd5eb72a" },
+      { name: "河北地区", url: "https://pan.quark.cn/s/b093c4bb3751" },
+      { name: "浙江地区", url: "https://pan.quark.cn/s/b72ca3480afa" },
+      { name: "港澳台", url: "https://pan.quark.cn/s/f5a64e95b2cc" },
+      { name: "西北地区", url: "https://pan.quark.cn/s/c35406eb2f22" },
+      { name: "东北地区", url: "https://pan.quark.cn/s/d6d7021aec5c" },
+      { name: "西南地区", url: "https://pan.quark.cn/s/086d5b241fb0" },
+      { name: "贵州地区", url: "https://pan.quark.cn/s/686d95aeaa35" },
+    ],
+  },
+  {
+    typeName: "G-套图合集",
+    typeList: [
+      { name: "G-套图合集", url: "https://pan.quark.cn/s/0b3f08d7053b" },
+      {
+        name: "徐若瑄天使三部曲",
+        url: "https://pan.quark.cn/s/4b5a18202761",
+      },
+      {
+        name: "徐若瑄3套绝版唯美写真",
+        url: "https://pan.quark.cn/s/e124bdce322b",
+      },
+      {
+        name: "Tu_20230910_085216_314",
+        url: "https://pan.quark.cn/s/57786edcf290",
+      },
+      { name: "SAZA-FLZY", url: "https://pan.quark.cn/s/72dd46a61c48" },
+      { name: "舒淇写真", url: "https://pan.quark.cn/s/4d01db3d1f23" },
+    ],
+  },
+  // {
+  //   typeName: "秀人网合集-XIUREN",
+  //   typeList: [
+  //     {
+  //       name: "秀人网合集-XIUREN",
+  //       url: "https://pan.quark.cn/s/da5323e2535c",
+  //     },
+  //   ],
+  // },
+  {
+    typeName: "T-听书集合",
+    typeList: [
+      { name: "T-听书集合", url: "https://pan.quark.cn/s/162db5481ab8" },
+
+      {
+        name: "林海雪原.全45回",
+        url: "https://pan.quark.cn/s/6abfdb6b274e",
+      },
+      {
+        name: "抗日英雄杨靖宇.全70回",
+        url: "https://pan.quark.cn/s/99b784be3e1b",
+      },
+      { name: "平原枪声", url: "https://pan.quark.cn/s/1975b16dab64" },
+      {
+        name: "敌后武工队.全36回-袁阔成",
+        url: "https://pan.quark.cn/s/b083953df821",
+      },
+      {
+        name: "少林将军许世友 全100回-单田芳",
+        url: "https://pan.quark.cn/s/e8853499c7e7",
+      },
+      {
+        name: "贺龙传奇.全300回-单田芳",
+        url: "https://pan.quark.cn/s/446aa743acfe",
+      },
+      {
+        name: "一代儒将陈毅.全90回",
+        url: "https://pan.quark.cn/s/82e047a5277f",
+      },
+      {
+        name: "十大传奇司令员大结局-35回",
+        url: "https://pan.quark.cn/s/997e71103194",
+      },
+      { name: "Ting", url: "https://pan.quark.cn/s/427671c50758" },
+    ],
+  },
+  {
+    typeName: "G-套图合集",
+    typeList: [
+      { name: "G-套图合集", url: "https://pan.quark.cn/s/0b3f08d7053b" },
+      {
+        name: "徐若瑄天使三部曲",
+        url: "https://pan.quark.cn/s/4b5a18202761",
+      },
+      {
+        name: "徐若瑄3套绝版唯美写真",
+        url: "https://pan.quark.cn/s/e124bdce322b",
+      },
+      {
+        name: "Tu_20230910_085216_314",
+        url: "https://pan.quark.cn/s/57786edcf290",
+      },
+      { name: "SAZA-FLZY", url: "https://pan.quark.cn/s/72dd46a61c48" },
+      { name: "舒淇写真", url: "https://pan.quark.cn/s/4d01db3d1f23" },
+    ],
+  },
+  // {
+  //   typeName: "1000首音乐",
+  //   typeList: [
+  //     {
+  //       name: "1000首音乐",
+  //       url: "https://pan.baidu.com/s/1UZWW-9WdoUnWMfF1bCU4BA?pwd=1024",
+  //     },
+  //   ],
+  // },
+  {
+    typeName: "杂记",
+    typeList: [
+      
+
+      { name: "VPN-合集", url: "https://ume.la/zdRyCd" },
+
+      { name: "中转站", url: "https://www.kdocs.cn/l/cjgFCNafyYnr" },
+
+      { name: "A-程序员合集", url: "https://pan.quark.cn/s/4de6c06d7daa" },
+      {
+        name: "60多门编程语言学习书籍超级大合集（700+本PDF）",
+        url: "https://pan.quark.cn/s/3f14b684b7e5",
+      },
+
+      { name: "F-付费模版", url: "https://pan.quark.cn/s/c6621ad7610d" },
+      { name: "T-工具集", url: "https://pan.quark.cn/s/6417932c1ca0" },
+      { name: "Y-压缩文档PDF", url: "https://pan.quark.cn/s/f4956dc64167" },
+    ],
+  },
+];
