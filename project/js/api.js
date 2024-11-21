d3815f153c13388faaffb7883b624f3ec071ef9f
SunDongYang <sundongyang1221@163.com>
2024-11-21 17:22:20 +0800

    huanghe

---
+++ project/js/api.js
@@ -0,0 +1,5 @@
+var API = {
+    water_video: "https://cdn.digitalcnzz.com/static/upload/luban/pages/luban_shuilushijiao\'.html",
+    air_video: 'https://cdn.digitalcnzz.com/static/upload/luban/pages/luban_kongzhingshijiao.html',
+    road_video: 'https://cdn.digitalcnzz.com/static/upload/luban/pages/luban_lulushijiao.html',
+}
